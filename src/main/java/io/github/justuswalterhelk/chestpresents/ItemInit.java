ea31a5aad53c0f578b23e95d8f8008a618cbaaa0
Justus Walter-Helk <75504335+JustusWalter-Helk@users.noreply.github.com>
2022-11-21 20:43:13 +0100

    cleanup
    TODO: missing texture, wrapping_paper

--- src/main/java/io/github/justuswalterhelk/chestpresents/ItemInit.java
+++ src/main/java/io/github/justuswalterhelk/chestpresents/ItemInit.java
@@ -3,7 +3,6 @@ package io.github.justuswalterhelk.chestpresents;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -13,4 +12,5 @@ public class ItemInit
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MODID);
 
     public static final RegistryObject<BlockItem> present_item = ITEMS.register("present_block", () -> new BlockItem(BlockInit.presentBlock.get(), new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> wrapping_paper = ITEMS.register("wrapping_paper", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 }
---
+++ src/main/java/io/github/justuswalterhelk/chestpresents/events/E_PlayerInteract.java
@@ -0,0 +1,66 @@
+package io.github.justuswalterhelk.chestpresents.events;
+
+
+import io.github.justuswalterhelk.chestpresents.BlockInit;
+import io.github.justuswalterhelk.chestpresents.ItemInit;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.ChestBlockEntity;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
+
+public class E_PlayerInteract
+{
+    @SubscribeEvent
+    public void PlayerInteract(PlayerInteractEvent.RightClickBlock event)
+    {
+        Level level = event.getLevel();
+        BlockPos position = event.getPos();
+        Player player = event.getEntity();
+
+        ItemStack blockToPlace = event.getItemStack();
+
+        if(blockToPlace.is(BlockInit.presentBlock.get().asItem()))
+        {
+            CompoundTag tag = blockToPlace.getTag();
+            BlockEntity block1 =  level.getBlockEntity(position.above());
+        }
+        else
+        {
+            BlockEntity block = level.getBlockEntity(position);
+            try {
+                ChestBlockEntity chest = (ChestBlockEntity) block;
+                if (chest != null && player.isCrouching() && event.getItemStack().is(ItemInit.wrapping_paper.get().asItem()) ) {
+                    InvWrapper handler = (InvWrapper) chest.getCapability(ForgeCapabilities.ITEM_HANDLER).orElseThrow(RuntimeException::new);
+                    ItemStackHandler inv = new ItemStackHandler(handler.getSlots());
+                    for(int i = 0; i < handler.getSlots(); i++)
+                    {
+                        inv.setStackInSlot(i, handler.getStackInSlot(i));
+                    }
+                    CompoundTag tag = inv.serializeNBT();
+                    chest.clearContent();
+                    level.destroyBlock(position, false);
+                    ItemStack present = new ItemStack(ItemInit.present_item.get());
+
+
+                    present.setTag(tag);
+
+                    ItemEntity item = new ItemEntity(level, position.getX(), position.getY(), position.getZ(), present);
+                    level.addFreshEntity(item);
+                }
+            }
+            catch (Exception ignored) {}
+        }
+
+
+    }
+}
